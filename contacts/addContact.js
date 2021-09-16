3a4a17aa01f4498a98cdb61dc2d4fd95060c1df1
TereshchenkoSD <serghh1988@gmail.com>
2021-09-16 15:26:26 +0300

    Удалил закомментированный код в функции addContact.js

--- contacts/addContact.js
+++ contacts/addContact.js
@@ -14,20 +14,4 @@ async function addContact(name, email, phone) {
   }
 }
 
-// const listContacts = require("./listContacts");
-// const updateContacts = require("./updateContacts");
-// const { v4 } = require("uuid");
-
-// async function addContact(name, email, phone) {
-//   try {
-//     const newContact = { id: v4(), name, email, phone };
-//     const contacts = await listContacts();
-//     contacts.push(newContact);
-//     await updateContacts(contacts);
-//     return newContact;
-//   } catch (error) {
-//     throw error;
-//   }
-// }
-
 module.exports = addContact;
